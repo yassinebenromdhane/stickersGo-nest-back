280cbb9fb95ed4a27262dd086bf046fe77b31bda
Yassine <medyassinebenromdhane00@gmail.com>
2023-07-04 15:30:15 +0100

    add PORT to env

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
 
 }
